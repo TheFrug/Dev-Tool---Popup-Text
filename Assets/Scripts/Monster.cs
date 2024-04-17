1021ef2116a26654c492d0ec322cc302b1dbc0be
Michael Cabrera <Michael.cabrera.0515@gmail.com>
2024-04-16 21:48:11 -0500

    Project03B

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField]
+    private MonsterData _data;
+    public MonsterData Data => _data;
+
+    private void Awake()
+    {
+        Debug.Log("Name: " + _data.Name);
+        Debug.Log("Damage: " + _data.Damage);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _data.RangeOfAwareness);
+
+        Gizmos.color = Color.cyan;
+        Vector3 ray = transform.forward * _data.RangeOfAwareness;
+        Gizmos.DrawRay(transform.position, ray);
+    }
+}
